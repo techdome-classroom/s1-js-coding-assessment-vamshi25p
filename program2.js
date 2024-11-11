21520dc451e95bd86d677938987e23595835e59f
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:15:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,7 +3,13 @@ const decodeTheRing = function (s, p) {
     // write your code here
     const m=s.length;
     const n=p.length;
-    const dp=Array(n+1).fill(false).map(()=>)
+    const dp=Array(n+1).fill(false).map(()=>Array(m+1).fill(false));
+
+    dp[0][0]=true; 
+
+    for(let i=1;i<=n;i++){
+      if(key[i-1])
+    }
 
   };
   
