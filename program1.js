f7d2c372135f1904a32af5e18e07aaabb7d247db
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:09:40 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,7 +5,7 @@ const getTotalIsles = function (grid) {
   let count=0;
   const dfs=(i,j)=>{
     if(i<0||i>=rows||j<0||j>=cols||grid[i][j]==='W') return;
-    grid[i][j]='0';
+    grid[i][j]='W';
     dfs(i+1,j);
     dfs(i-1,j);
     dfs(i,j+1);
