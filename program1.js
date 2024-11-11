9625066f4b63c1873390f0df6c31f778f828c9f2
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:10:04 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,27 +1,27 @@
 const getTotalIsles = function (grid) {
-  if(!grid||grid.length===0) return 0;
-  const rows=grid.length;
-  const cols=grid[0].length;
-  let count=0;
-  const dfs=(i,j)=>{
-    if(i<0||i>=rows||j<0||j>=cols||grid[i][j]==='W') return;
-    grid[i][j]='W';
-    dfs(i+1,j);
-    dfs(i-1,j);
-    dfs(i,j+1);
-    dfs(i,j-1);
+  if (!grid || grid.length === 0) return 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
+  const dfs = (i, j) => {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === "W") return;
+    grid[i][j] = "W";
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
   };
-  for(let i=0;i<rows;i++){
-    for(let j=0;j<cols;j++){
-      if(grid[i][j]==='L'){
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === "L") {
         count++;
-        dfs(i,j);
+        dfs(i, j);
       }
     }
   }
+  return count;
 
   // write your code here
-
 };
 
 module.exports = getTotalIsles;
