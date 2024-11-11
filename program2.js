75ac43c80724e4bb335677a26d350f3521961670
vamshi25p <vamshikrishna788sai@gmail.com>
2024-11-11 13:16:34 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,28 +1,29 @@
 const decodeTheRing = function (s, p) {
+  // write your code here
+  const m = s.length;
+  const n = p.length;
+  const dp = Array(n + 1)
+    .fill(false)
+    .map(() => Array(m + 1).fill(false));
 
-    // write your code here
-    const m=s.length;
-    const n=p.length;
-    const dp=Array(n+1).fill(false).map(()=>Array(m+1).fill(false));
+  dp[0][0] = true;
 
-    dp[0][0]=true; 
+  for (let i = 1; i <= n; i++) {
+    if (key[i - 1] === "*") {
+      dp[i][0] = dp[i - 1][0];
+    }
+  }
 
-    for(let i=1;i<=n;i++){
-      if(key[i-1]==='*'){
-        dp[i][0]=dp[i-1][0];
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; j++) {
+      if (p[i - 1] === "*") {
+        dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+      } else if (p[i - 1] === "?" || p[i - 1] === s[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
       }
     }
+  }
+  return dp[n][m];
+};
 
-    for(let i=1;i<=n;i++){
-      for(let j=1;j<=m;j++){
-        if(p[i-1]==='*'){
-          dp[i][j]=dp[i-1][j]||dp[i][j-1];
-        }else if(p[i-1]==='?'||p[i-1]===s[j-1]){
-          dp[i][j]=dp[i-1][j-1];
-        }
-      }
-    }
-
-  };
-  
-  module.exports = decodeTheRing;
+module.exports = decodeTheRing;
